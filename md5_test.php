67935bb0ab3b020ba71ed4c93e2dade319e2b370
Moh-Arriza <moh-arriza-n@users.noreply.github.com>
2020-01-01 22:03:35 +0700

    bootstrap testing OK

--- md5_test.php
+++ md5_test.php
@@ -1,3 +1,16 @@
+<html>
+<head>
+	<title>BOOTSTRAP TEST</title>
+	<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap.css">
+	<script type="text/javascript" src="Bootstrap/js/jquery.js"></script>
+	<script type="text/javascript" src="Bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
 <?php
-echo md5('Hello Moto');
+echo md5('Wedew');
 ?>
+	<h1>TEST BUTTON</h1>
+	<button class="btn btn-danger">Exit</button>
+	<button class="btn btn-primary">Enter</button>
+</body>
+</html>
