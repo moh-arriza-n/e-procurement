8922bced9ed02513317daa8d051aafc81c555394
Moh-Arriza <moh-arriza-n@users.noreply.github.com>
2020-01-01 21:44:02 +0700

    listing program belum selesai

--- login-page.php
+++ login-page.php
@@ -1,9 +1,26 @@
+<!DOCTYPE html>
 <html>
 	<head>
 	<title>Log-in Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 		<body bgcolor="grey">
-		<h1><Font color="white">E-Procurement</font></h1>
+		<h1><Font color="white">E-Procurement</font></h1
+		<br/>
+			<div class="login">
+		<br/>
+			<form action="login.php" method="post" onSubmit="return validasi()">
+				<label>Username:</lable>
+				<input type="text" name="username id="username" />
+			</div>
+			<div>
+				<label>Password:</label>
+				<input type="password name="password"id="password" />
+			</div>
+			<div>
+				<input type="submit" value="login" class="tombol">
+			</div>
+			
 <?php
 
 ?>
